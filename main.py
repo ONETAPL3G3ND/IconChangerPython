723895c49689ca12cc540eea6775547a0682b7b7
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-05-15 22:36:42 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import win32com.client
+
+def change_icon(shortcut_path, icon_path):
+    shell = win32com.client.Dispatch("WScript.Shell")
+    shortcut = shell.CreateShortcut(shortcut_path)
+    shortcut.IconLocation = icon_path
+    shortcut.Save()
+
+# Пример использования:
+shortcut_path = "C:\\Users\\User\\Desktop\\MyShortcut.lnk"
+icon_path = "C:\\Path\\To\\Icon.ico"
+change_icon(shortcut_path, icon_path)
